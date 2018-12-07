26dad156ad2aa5a8795a22ffd668927f2134f512
Henri Bialek <il1k3pepsi@gmail.com>
2018-12-07 16:30:32 +0100

    Update API description and implementation

--- src/main/java/com/example/restful/api/impl/PetsApiServiceImpl.java
+++ src/main/java/com/example/restful/api/impl/PetsApiServiceImpl.java
@@ -9,10 +9,7 @@ import com.example.restful.model.Pet;
 import java.util.ArrayList;
 import java.util.List;
 import com.example.restful.api.NotFoundException;
-
-import java.io.InputStream;
-
-import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
@@ -29,21 +26,69 @@ public class PetsApiServiceImpl extends PetsApiService {
         return leftLimit + (long) (Math.random() * (rightLimit - leftLimit));
     }
 
-    @Override
-    public Response createPets(SecurityContext securityContext) throws NotFoundException {
+    private Pet petId() {
+        return new Pet().id(generateId());
+    }
 
-        pets.add(new Pet().id(generateId()).name("Joey").tag("dog"));
-        pets.add(new Pet().id(generateId()).name("MaryLou").tag("cat"));
-        pets.add(new Pet().id(generateId()).name("Rico").tag("budgie"));
+    @Override
+    public Response createPet(PetPrototype petPrototype, SecurityContext securityContext) throws NotFoundException {
+
+        if (petPrototype == null) {
+            Error errorResponse = new Error().code(400).message("Request body must not be empty and represent the PetPrototype.");
+
+            return Response.status(400).entity(errorResponse).build();
+        }
+
+        Pet p = new Pet().id(generateId());
+        p.setName(petPrototype.getName());
+        p.setTag(petPrototype.getTag());
+        pets.add(p);
 
         // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        return Response.ok().entity(null).build();
     }
+
     @Override
-    public Response listPets( Integer limit, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+    public Response getPets( Integer limit, Integer index, SecurityContext securityContext) throws NotFoundException {
+
+        List<Pet> responsePayload = null;
+        String nextPageLink = StringUtils.EMPTY;
+
+        // set default index
+        if (index == null) {
+            index = 0;
+        }
+
+        // set default limit
+        if (limit == null) {
+            limit = 100;
+        }
+
+        // handle empty list
+        if (pets.isEmpty()) {
+            return Response.ok().build();
+        }
+
+        // handle index too large
+        if (index > pets.size()) {
+            Error e = new Error().code(1).message("Index exceeds size of listed pets.");
+            return Response.status(400).entity(e).build();
+        }
+
+        // handle any page in valid range
+        if (pets.size() >= (index + limit)) {
+            responsePayload = pets.subList(index, (index + limit));
+        }
+
+        // handle last page
+        if (pets.size() < (index + limit)) {
+            responsePayload = pets.subList(index, (index + (pets.size() % index)));
+        }
+
+        return Response.ok().entity(responsePayload).header(
+                "X-next", "localhost:9090/api/pets" + "?limit=" + limit + "&index=" + (limit + index)).build();
     }
+
     @Override
     public Response showPetById(String petId, SecurityContext securityContext) throws NotFoundException {
         // do some magic!
